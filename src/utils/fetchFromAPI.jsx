727f52a4a82beab3688b85c3ed5ca2f5eb5f07a7
iamClutchX <iamr.karan@gmail.com>
2022-08-30 03:32:54 +0530

    first commit

---
+++ src/utils/fetchFromAPI.jsx
@@ -0,0 +1,19 @@
+import axios from 'axios';
+
+export const BASE_URL = 'https://youtube-v31.p.rapidapi.com';
+
+const options = {
+  params: {
+    maxResults: 50,
+  },
+  headers: {
+    'X-RapidAPI-Key':'806e8e51fbmshd10d124bfd1947fp1c8d12jsn044e04d49402',
+    'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com'
+  },
+};
+
+export const fetchFromAPI = async (url) => {
+  const { data } = await axios.get(`${BASE_URL}/${url}`, options);
+
+  return data;
+};
